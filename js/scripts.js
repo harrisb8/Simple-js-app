6fb9dd34c51847f0036bf5ce3ef27a6ffcc1c859
harrisb8 <harriskbrandon89@gmail.com>
2021-06-02 09:24:28 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+/* playing around with the code */
+let simpleAddition= 3+5 ;
+document.write(simpleAddition);
+
+let size= 100;
+let doubleSize= size*2;
+let miniSize= (doubleSize *2) - (size / 2);
+document.write(miniSize)
