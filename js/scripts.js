535f47b64bc0f737c949d9c62cbd733ef5bc61f3
harrisb8 <harriskbrandon89@gmail.com>
2021-06-02 12:31:41 -0400

    pokemonList
    Deleted practice work to start a new project (pokemonList)

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-alert('Hello World');
-
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-
-/* playing around with the code */
-let simpleAddition= 3+5 ;
-document.write(simpleAddition);
-
-let size= 100;
-let doubleSize= size*2;
-let miniSize= (doubleSize *2) - (size / 2);
-document.write(miniSize)
-
-var favoriteFood = 'Fried Rice' ;
-document.write(favoriteFood);
+//establishing my pokemonList and important attributes//
+let pokemonList = [
+   name: ,
+   height: ,
+   type: ,
+   strength: ,
+   speed: ,
+   weakness: ;   
+];
